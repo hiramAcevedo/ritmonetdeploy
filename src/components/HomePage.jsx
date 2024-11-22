c1709d8b716c9e7b29501b77aed370ec27305075
hiramAcevedo <hiram.acevedo.lopez@gmail.com>
2024-11-21 18:32:31 -0600

    Prueba de ruta 3

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -10,7 +10,7 @@ const HomePage = () => {
   return (
     <div>
       <Hero
-        backgroundImage="public/assets/img/HomePage/pexels-photo-164821.jpg"
+        backgroundImage="/assets/img/HomePage/pexels-photo-1648219.jpg"
         title="Aprende Música en Línea"
         subtitle="Domina tu instrumento favorito desde cualquier lugar del mundo"
         buttonText="Comenzar"
